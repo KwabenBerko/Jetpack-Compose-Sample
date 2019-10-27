a4730fb45ead7df8c5f770fcf8dc554a7ae1f915
Kwabena Bio Berko <kwabena@farmerline.co>
2019-10-27 13:06:22 +0000

    Initial Commit

---
+++ app/src/main/java/com/kwabenaberko/jetpack_compose_sample/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.kwabenaberko.jetpack_compose_sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.Text
+import androidx.ui.core.setContent
+import androidx.ui.material.MaterialTheme
+import androidx.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Hello World!")
+        }
+    }
+}
+
